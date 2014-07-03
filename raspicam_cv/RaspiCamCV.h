bf3964e2e3cb94affbc2fa6db5318e0f4d6d910c
nicjohnston <nijo5747@colorado.edu>
2014-07-02 23:13:25 -0600

    Update RaspiCamCV.h

--- raspicam_cv/RaspiCamCV.h
+++ raspicam_cv/RaspiCamCV.h
@@ -19,12 +19,7 @@ void raspiCamCvSetCaptureProperty(RaspiCamCvCapture * capture, int property_id, 
 void printEncoder(RaspiCamCvCapture * capture);
 IplImage * raspiCamCvQueryFrame(RaspiCamCvCapture * capture);
 IplImage * raspiCamCvQueryFrame_New(RaspiCamCvCapture * capture, int mode);
-void raspiCamCvQueryFrame_New_Three(RaspiCamCvCapture * capture, IplImage * Y, IplImage * U, IplImage * V);
 IplImage * raspiCamCvQueryFrame_New_Final(RaspiCamCvCapture * capture);
-/*void raspiCamCvQueryFrame_New_Start(RaspiCamCvCapture * capture);//, Mat& Y, Mat& U, Mat& V);
-IplImage * raspiCamCvQueryFrame_New_Y(RaspiCamCvCapture * capture);
-IplImage * raspiCamCvQueryFrame_New_U(RaspiCamCvCapture * capture);
-IplImage * raspiCamCvQueryFrame_New_V(RaspiCamCvCapture * capture);*/
 
 #ifdef __cplusplus
 }
