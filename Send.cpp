2e69fb3ae7ea84e09b6815edd1eb80d55030cfab
nicjohnston <nijo5747@colorado.edu>
2014-07-08 02:08:45 -0600

    Update Send.cpp

--- Send.cpp
+++ Send.cpp
@@ -98,36 +98,4 @@ int main(int argc, char *argv[])
 	//imshow( "Client", img);  
 	waitKey(0);
 	return 0;
-}/*
-	int sock_out;
-	int msgL = 0;
-	struct sockaddr_in server_addr_out;
-	struct hostent *host_out;
-	char send_data[1024];
-
-
-	// client config
-	host_out = (struct hostent *) gethostbyname((char *) "127.0.0.1");
-
-	if ((sock_out = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-		perror("socket");
-		exit(1);
-	}
-
-	server_addr_out.sin_family = AF_INET;
-	server_addr_out.sin_port = htons(5010);
-	server_addr_out.sin_addr = *((struct in_addr *) host_out->h_addr);
-	bzero(&(server_addr_out.sin_zero), 8);
-
-	// wait for new UDP packet, and read it in to recv_data
-	bytes_read = recvfrom(sock_in, recv_data, 1024, 0, (struct sockaddr *) &client_addr_in, &addr_len);
-	recv_data[bytes_read] = '\0';
-
-	// Construct a packet to send over UDP with control inputs	  
-	msgL = sprintf(send_data, "%f\t%f\t%f\t%f\t%f\n", aileron_ctrl, pitch_ctrl, rudder_ctrl, thr_ctrl, thr_ctrl);
-	// Print Control inputs to stdout
-	printf("\nControl Signal\n%s\n", send_data);
-	cout << "\nWPN: " << wpn << endl;
-	// Send UDP packet
-	sendto(sock_out, send_data, strlen(send_data), 0, (struct sockaddr *) &server_addr_out, sizeof (struct sockaddr));
-*/
+}
